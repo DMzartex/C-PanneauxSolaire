20cc8c491f346d31293074a027cabea3b1a6dc36
DMzartex <91669692+DMzartex@users.noreply.github.com>
2021-09-30 22:32:49 +0200

    Ajout du programme panneaux solaire

---
+++ C#PanneauxSolaire/panneauxSolaire/panneauxSolaire/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace panneauxSolaire
+{
+    class Program
+    {
+     static void panneauxSolaireCalc(double largM, double longM, double hc, double hp, out double nbPanneaux, out double prixTotal)
+        {
+            double versantToit, npL, npH;
+            double lPan = 0.7;
+            double hPan = 1.5;
+            double prixPan = 489;
+
+
+            versantToit = Math.Pow(Math.Pow((hp - hc), 2) + Math.Pow((largM / 2), 2), 0.5);
+            npL = longM / lPan;
+            npH = versantToit / hPan;
+            nbPanneaux = npL * npH;
+            prixTotal = nbPanneaux * prixPan;
+            
+        }
+        static void Main(string[] args)
+        {
+            double largM, longM, hc, hp, nbPanneaux, prixTotal;
+            Console.WriteLine("Entrez la largeur de la maison svp :");
+            largM = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez la longueur de la maison svp :");
+            longM = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez la hauteur à la corniche");
+            hc = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez la hauteur au pignon");
+            hp = double.Parse(Console.ReadLine());
+
+
+            //appel au traitement
+
+            panneauxSolaireCalc(largM, longM, hc, hp, out nbPanneaux, out prixTotal);
+
+            // Affichage
+            Console.WriteLine($"le nombre de panneaux est de : {Math.Ceiling(nbPanneaux)}");
+            Console.WriteLine($"le prix totale est de :{Math.Ceiling(prixTotal)}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
